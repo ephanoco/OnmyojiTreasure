7d93031dadd4b4b05f625c7c8dd460cf984f6d40
Samuel <3ph4n0c0@gmail.com>
2023-07-24 05:09:07 +0800

    fix: Cooldown handler

--- modules/realm_raider.py
+++ modules/realm_raider.py
@@ -20,31 +20,32 @@ class RealmRaider(Matcher, Cursor):
         is_individual = len(super().match(
             super().get_path(f'{self.rel_path}individual_active.png'), thresh_mul=0.98
         )) != 0
-        self.__individual_raid() if is_individual else self.__guild_raid(is_cooldown=is_cooldown)
+        self.__individual_raid() if is_individual else self.__guild_raid(is_cooldown)
 
-    def __guild_raid(self, index: str = '', is_cooldown=False):
+    def __guild_raid(self, is_cooldown, index: str = ''):
         # Choose the realm
         scatter = self.__get_guild_scatter()
         cur_index = self.guild_defeated_count if not index else int(index)
         super().left_click(scatter[cur_index], (1, 2))
 
-        pt_raid = super().match(super().get_path(f'{self.rel_path}raid.png'), is_multiple=False,
-                                thresh_sgl=0.02, is_with_colour=True)
-        print(f'pt_raid: {pt_raid}')
-        # Cooldown
-        if is_cooldown and not pt_raid:
-            time.sleep(1800)
-            return self.__guild_raid()
-        # Successful penetration
-        if not pt_raid:
-            return
+        pt_raid_list = super().match(super().get_path(f'{self.rel_path}raid.png'),
+                                     thresh_mul=0.92) if not is_cooldown else super().match(
+            super().get_path(f'{self.rel_path}raid.png'), is_multiple=False,
+            thresh_sgl=0.02, is_with_colour=True)
+        print(f'pt_raid_list: {pt_raid_list}')
+        if not pt_raid_list:
+            if not is_cooldown:  # Successful penetration
+                return
+            else:  # Cooldown
+                time.sleep(1800)
+                return self.__guild_raid(True)
 
-        super().left_click(pt_raid, 2)  # Raid
+        super().left_click(pt_raid_list[0], 2)  # Raid
         # The realm has been raided
         pt_raid_list = super().match(super().get_path(f'{self.rel_path}raid.png'), thresh_mul=0.92)
         if pt_raid_list:
             super().left_click(scatter[cur_index + 1], (1, 2))
-            return self.__guild_raid(str(cur_index + 1))
+            return self.__guild_raid(is_cooldown, str(cur_index + 1))
 
         time.sleep(2)
         self.__mark_ghost()
@@ -60,14 +61,14 @@ class RealmRaider(Matcher, Cursor):
             if pt_victory_list:
                 time.sleep(1)
                 super().left_click(pt_victory_list[0], (4, 5))
-                self.__guild_raid()
+                self.__guild_raid(is_cooldown)
                 break
             pt_defeat_list = super().match(super().get_path(f'{self.rel_path}defeat.png'), False)
             if pt_defeat_list:
                 super().left_click(pt_defeat_list[0], (4, 5))
                 self.guild_defeated_count += 1
                 if self.guild_defeated_count <= len(scatter):
-                    self.__guild_raid()
+                    self.__guild_raid(is_cooldown)
                 break
 
     def __mark_ghost(self, pt=(), times=0):
@@ -107,7 +108,7 @@ class RealmRaider(Matcher, Cursor):
     def __get_guild_scatter(self):
         scatter = pt_dict['explore_map']['realm_raid']['guild']['scatter']
         if not scatter:
-            x, y = super().match(super().get_path(f'{self.rel_path}buffs.png'))[0]
+            x, y = super().match(super().get_path(f'{self.rel_path}buffs.png'), thresh_mul=0.97)[0]
             scatter = pt_dict['explore_map']['realm_raid'][
                 'guild']['scatter'] = [(x + 437, y + 34), (x + 707, y + 34), (x + 437, y + 142), (x + 707, y + 142),
                                        (x + 437, y + 250), (x + 707, y + 250)]
